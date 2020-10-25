8b41dd643fadff1d254a900d7b544506156f622a
Panagiotis Kolokotronis <panagiotis.kolokotronis@noris.gr>
2020-10-25 21:19:51 +0200

    No need to re-export namespaces

--- opium/__init__.py
+++ opium/__init__.py
@@ -3,8 +3,3 @@
 __author__ = """Kolokotronis Panagiotis"""
 __email__ = "panagiks@gmail.com"
 __version__ = "0.1.0"
-
-from opium import business
-from opium import config
-from opium import okd
-from opium import opium
